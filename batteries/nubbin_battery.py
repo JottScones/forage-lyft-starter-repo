3cab19d51d20a31768ac2d9124f1ad50285b74ad
Scott Jones <loopyscoot@gmail.com>
2022-05-08 15:27:31 +0100

    reordered constructors for components to match the pdf model spec

--- batteries/nubbin_battery.py
+++ batteries/nubbin_battery.py
@@ -3,9 +3,9 @@ from battery import Battery
 class NubbinBattery(Battery):
     """Implementation of the Nubbin Battery class."""
 
-    def __init__(self, last_service_date, current_date):
-        self.last_service_date = last_service_date
+    def __init__(self, current_date,  last_service_date):
         self.current_date      = current_date
+        self.last_service_date = last_service_date
 
     def needs_service(self):
         last_service_year      = self.last_service_date.year
