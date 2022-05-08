96def943ea50bfefbc9aaff1c8bd4d46c82d6dcb
Scott Jones <loopyscoot@gmail.com>
2022-05-08 15:33:59 +0100

    Created structural outline for car factory methods

---
+++ car_factory.py
@@ -0,0 +1,18 @@
+from car import Car
+from engines.capulet_engine import CapuletEngine
+from engines.sternman_engine import SternmanEngine
+from engines.willoughby_engine import WilloughbyEngine
+
+from batteries.nubbin_battery import NubbinBattery
+from batteries.spindler_battery import SpindlerBattery
+
+class CarFactory(object):
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
+
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
+
+    def create_palindrome(current_date, last_service_date, warning_light_is_on):
+
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
+
+    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
