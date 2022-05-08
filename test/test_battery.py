6030d40fd7d91f1f939d26e5f4cc42dd0f059c51
Scott Jones <loopyscoot@gmail.com>
2022-05-08 22:32:19 +0100

    segmented unit tests by component

---
+++ test/test_battery.py
@@ -0,0 +1,39 @@
+import unittest
+from datetime import datetime
+
+from batteries.nubbin_battery import NubbinBattery
+from batteries.spindler_battery import SpindlerBattery
+
+class TestSpindlerBattery(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        current_date      = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 3)
+
+        spindler_battery = SpindlerBattery(current_date, last_service_date)
+        self.assertTrue(spindler_battery.needs_service(),
+        "Battery with more than 2 years since last service should need a service.")
+
+    def test_battery_should_not_be_serviced(self):
+        current_date      = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 1)
+
+        spindler_battery = SpindlerBattery(current_date, last_service_date)
+        self.assertFalse(spindler_battery.needs_service(),
+        "Battery with fewer than 2 years since last service should not need a service.")
+
+class TestNubbinBattery(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        current_date      = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 5)
+
+        nubbin_battery = NubbinBattery(current_date, last_service_date)
+        self.assertTrue(nubbin_battery.needs_service(),
+        "Battery with more than 4 years since last service should need a service.")
+
+    def test_battery_should_not_be_serviced(self):
+        current_date      = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 3)
+
+        nubbin_battery = NubbinBattery(current_date, last_service_date)
+        self.assertFalse(nubbin_battery.needs_service(),
+        "Battery with fewer than 4 years since last service should not need a service.")
--- test/test_engine.py
+++ test/test_engine.py
@@ -1,13 +1,8 @@
 import unittest
-from datetime import datetime
 
 from engines.capulet_engine import CapuletEngine
 from engines.sternman_engine import SternmanEngine
 from engines.willoughby_engine import WilloughbyEngine
-
-from batteries.nubbin_battery import NubbinBattery
-from batteries.spindler_battery import SpindlerBattery
-
 
 class TestCapuletEngine(unittest.TestCase):
     def test_engine_should_be_serviced(self):
@@ -57,41 +52,3 @@ class TestWilloughbyEngine(unittest.TestCase):
         willoughby_engine = WilloughbyEngine(current_mileage, last_service_mileage)
         self.assertFalse(willoughby_engine.needs_service(),
         "Engines that have done less than 30k miles should not need a service.")
-
-class TestSpindlerBattery(unittest.TestCase):
-    def test_battery_should_be_serviced(self):
-        current_date      = datetime.today().date()
-        last_service_date = current_date.replace(year=current_date.year - 3)
-
-        spindler_battery = SpindlerBattery(current_date, last_service_date)
-        self.assertTrue(spindler_battery.needs_service(),
-        "Battery with more than 2 years since last service should need a service.")
-
-    def test_battery_should_not_be_serviced(self):
-        current_date      = datetime.today().date()
-        last_service_date = current_date.replace(year=current_date.year - 1)
-
-        spindler_battery = SpindlerBattery(current_date, last_service_date)
-        self.assertFalse(spindler_battery.needs_service(),
-        "Battery with fewer than 2 years since last service should not need a service.")
-
-class TestNubbinBattery(unittest.TestCase):
-    def test_battery_should_be_serviced(self):
-        current_date      = datetime.today().date()
-        last_service_date = current_date.replace(year=current_date.year - 5)
-
-        nubbin_battery = NubbinBattery(current_date, last_service_date)
-        self.assertTrue(nubbin_battery.needs_service(),
-        "Battery with more than 4 years since last service should need a service.")
-
-    def test_battery_should_not_be_serviced(self):
-        current_date      = datetime.today().date()
-        last_service_date = current_date.replace(year=current_date.year - 3)
-
-        nubbin_battery = NubbinBattery(current_date, last_service_date)
-        self.assertFalse(nubbin_battery.needs_service(),
-        "Battery with fewer than 4 years since last service should not need a service.")
-
-
-if __name__ == '__main__':
-    unittest.main()
